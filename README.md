d9bfc45400773403fa1b16d84c17854f4ae4577c
Hannah A. Patellis <hpatellis@icloud.com>
2026-01-10 16:44:51 -0600

    First revision of app/get.php. Working. Includes README.md too

---
+++ README.md
@@ -0,0 +1,28 @@
+# Perfume Of The Day
+
+I love perfumes! And I log what I wear daily on [Parfumo](https://www.parfumo.com), a perfume community site I really love. This is a small PHP script that simply loads my Parfumo profile page and gets the current perfume, then loads the perfume's entry and gets the perfume name and perfume brand. Then it prints out JSON for easy inclusion on my website. Easy as that!
+
+Please don't abuse this script. I figure that with the very little traffic I get to my personal website, querying Parfumo twice with every visit to my "About" page isn't going to be any kind of taxing.
+
+## Packages Used
+
+- [hQuery.php](https://github.com/duzun/hQuery.php)
+  - Fast and efficient web scraper for PHP
+
+
+## Endpoint
+
+- `/get.php` GET request that returns JSON of the currently worn perfume
+```
+{
+  "brand": "String of perfume brand",
+  "perfume": "String of perfume name",
+  "url": "String of perfume URL on Parfumo"
+}
+```
+
+---
+
+Developed by Alexandria 'Hannah' I. Patellis, starting in 2026
+
+[hannahap.com](https://hannahap.com)
---
+++ app/get.php
@@ -0,0 +1,35 @@
+<?php
+
+$parfumoUsername = "iconicos";
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use duzun\hQuery;
+
+// Query your profile page
+$docProfile = hQuery::fromUrl('https://www.parfumo.com/Users/'.$parfumoUsername, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
+
+// Select perfume page URL from profile page
+$perfumeURL = $docProfile->find("div#i_wear_sidebar div.pgrid div.col div.name a")->attr('href');
+
+// Query the perfume page itself
+$docPerfume = hQuery::fromUrl($perfumeURL, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
+
+// Select the perfume name (and remove everything after the name) and the perfume brand
+// Trim whitespace from name and brand
+$perfumeName = $docPerfume->find("div.p_details_section h1.p_name_h1")->html();
+$perfumeName = strtok($perfumeName, "<");
+$perfumeName = trim($perfumeName);
+$perfumeBrand = $docPerfume->find("span.p_brand_name a:first-child span")->text();
+$perfumeBrand = trim($perfumeBrand);
+
+// Prepare an array of results
+$result = [
+    "brand" => $perfumeBrand,
+    "perfume" => $perfumeName,
+    "url" => $perfumeURL
+];
+
+// Print the array as JSON to page
+print_r(json_encode($result));
+
+?>
